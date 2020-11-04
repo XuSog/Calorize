6940cb28f109994e490133990e5f21bb5a8ab632
darthnoward <hhl055959@gmail.com>
2020-11-05 02:09:04 +0800

    add method for storing data

--- app/src/main/java/com/example/healthcare/MainActivity.java
+++ app/src/main/java/com/example/healthcare/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
@@ -13,22 +14,38 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton profileButton;
     ArrayList<Integer> parentLayout = new ArrayList<Integer>();
-    static String userName = "Name";
+    ArrayList<String> data = null;
     EditText nameUser;
+    EditText studentID;
     Button saveButton;
 
     @Override
     public void onBackPressed() {
-        if (parentLayout.isEmpty()) return;
-        changeLayout(parentLayout.get(parentLayout.size()-1));
-        parentLayout.remove(parentLayout.get(parentLayout.size()-1));
+        if (parentLayout.isEmpty()) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Exit?")
+                    .setMessage("Are you going to exit the app?")
+                    .setNegativeButton(android.R.string.no, null)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface arg0, int arg1) {
+                            MainActivity.super.onBackPressed();
+                        }
+                    }).create().show();
+        }
+        else {
+            changeLayout(parentLayout.get(parentLayout.size() - 1));
+            parentLayout.remove(parentLayout.get(parentLayout.size() - 1));
+        }
     }
 
     @Override
@@ -50,20 +67,54 @@ public class MainActivity extends AppCompatActivity {
                     changeLayout(R.layout.profile);
                 }
             });
-            TextView userNameInput = findViewById(R.id.userName);
-            userNameInput.setText(userName);
+            loadData();
+            TextView view = findViewById(R.id.userName);
+            view.setText(data.get(0));
+            view = findViewById(R.id.studentIDBox);
+            view.setText(data.get(1));
+
         }
-        
+
         if ( layout == R.layout.profile) {
             nameUser = findViewById(R.id.nameUser);
+            studentID = findViewById(R.id.studentID);
             saveButton = findViewById(R.id.saveButtonId);
             saveButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    MainActivity.userName = nameUser.getText().toString();
+                    if (! nameUser.getText().toString().isEmpty()) {
+                        data.set(0, nameUser.getText().toString());
+                    }
+                    if (! studentID.getText().toString().isEmpty()) {
+                        data.set(1, studentID.getText().toString());
+                    }
+                    saveData();
                 }
             });
         }
 
+    }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(data);
+        editor.putString("data list", json);
+        editor.apply();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("data list", null);
+        Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        data = gson.fromJson(json, type);
+
+        if (data == null || data.isEmpty()) {
+            data = new ArrayList<String>();
+            data.add("Name");
+            data.add("Student ID");
+        }
     }
 }
