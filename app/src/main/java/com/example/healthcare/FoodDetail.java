dcced3d66b802f493286b664204fd1201003a616
darthnoward <hhl055959@gmail.com>
2020-11-24 14:27:34 +0800

    add getting recommended data for user

--- app/src/main/java/com/example/healthcare/FoodDetail.java
+++ app/src/main/java/com/example/healthcare/FoodDetail.java
@@ -2,11 +2,15 @@ package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -19,7 +23,15 @@ import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 public class FoodDetail extends AppCompatActivity {
 
@@ -28,16 +40,31 @@ public class FoodDetail extends AppCompatActivity {
     BarDataSet barDataSet;
     ArrayList barEntries;
     Button cancelButton;
+    Button saveButton;
+    TextView name;
+    String[] data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_detail);
-        cancelButton = findViewById(R.id.cancelButton);
+        data = getData();
+        name = findViewById(R.id.name);
+        name.setText(data[0]);
+        cancelButton = findViewById(R.id.cancelButtonFD);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        saveButton = findViewById(R.id.saveButtonFD);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+                Toast.makeText(FoodDetail.this, "Selected",Toast.LENGTH_SHORT).show();
+
             }
         });
         barChart = findViewById(R.id.BarChart);
@@ -48,6 +75,10 @@ public class FoodDetail extends AppCompatActivity {
         xAxis.setValueFormatter(new MyXAxisFormatter());
         YAxis yAxis = barChart.getAxisLeft();
         yAxis.setTextSize(10f);
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(150f);
+        yAxis.setGranularity(150f);
+        yAxis.setLabelCount(7, true);
         yAxis.setTextColor(Color.WHITE);
         Legend legend = barChart.getLegend();
         legend.setEnabled(false);
@@ -69,10 +100,10 @@ public class FoodDetail extends AppCompatActivity {
 
     private void getEntries() {
         barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(1.5f, 2));
-        barEntries.add(new BarEntry(3f, 1));
-        barEntries.add(new BarEntry(4.5f, 5));
-        barEntries.add(new BarEntry(6f, 3));
+        barEntries.add(new BarEntry(1.5f, Integer.valueOf(data[1])));
+        barEntries.add(new BarEntry(3f, Integer.valueOf(data[2])));
+        barEntries.add(new BarEntry(4.5f, Integer.valueOf(data[3])));
+        barEntries.add(new BarEntry(6f, Integer.valueOf(data[4])));
     }
 
     class MyXAxisFormatter implements IAxisValueFormatter{
@@ -84,4 +115,78 @@ public class FoodDetail extends AppCompatActivity {
             return "Fat";
         }
     }
+
+    private void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("food data", null);
+        Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        ArrayList<String> foodData = gson.fromJson(json, type);
+        foodData.add(String.valueOf(Calendar.getInstance().getTime()));
+        foodData.add(data[1]);
+        json = gson.toJson(foodData);
+        editor.putString("food data", json);
+        editor.apply();
+        name.setText(String.valueOf(foodData.size()));
+        System.out.println(foodData);
+    }
+
+    // to be implemented
+    private String[] getData(){
+        Intent intent = getIntent();
+
+        String[] foodDetail = new String[]{"Pasta", "123", "13", "32", "232"};  //to be replaced with actual value
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("data list", null);
+        Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        ArrayList<String> data = gson.fromJson(json, type);
+        User user = new User(Double.valueOf(data.get(2)),Double.valueOf(data.get(3)), data.get(5),Integer.valueOf(data.get(4)));
+        String[] dataAdvised = user.advise();
+        String[] dataString = new String[4];
+        dataString[0] = String.valueOf(Integer.valueOf((int) (Double.valueOf(foodDetail[1])/Double.valueOf(dataAdvised[0]))));
+        dataString[1] = String.valueOf(Integer.valueOf((int) (Double.valueOf(foodDetail[2])/Double.valueOf(dataAdvised[1]))));
+        dataString[2] = String.valueOf(Integer.valueOf((int) (Double.valueOf(foodDetail[3])/Double.valueOf(dataAdvised[2]))));
+        dataString[3] = String.valueOf(Integer.valueOf((int) (Double.valueOf(foodDetail[4])/Double.valueOf(dataAdvised[3]))));
+        String[] output = Arrays.copyOf(dataString,dataString.length + 1);
+        output[0] = foodDetail[0];
+        System.arraycopy(dataString, 0, output, 1, dataString.length);
+        return output;
+    }
+
+
+    class User {
+        private double height,weight;
+        private String sex;
+        private int year;
+        User(double height,double weight,String sex,int year){
+            this.height=height;
+            this.weight=weight;
+            this.sex=sex;
+            this.year=year;
+
+        }
+        public String[] advise(){
+            String[] result=new String[4];
+            BigDecimal energy,protein,cholesterol,fat;
+
+            if (this.sex=="male"){
+                energy=new BigDecimal(66+13.7*this.weight+5*this.height-6.8*year);
+                protein=new BigDecimal(1.1*this.weight);
+            }else{
+                energy=new BigDecimal(655+9.6*this.weight+1.8*this.height-4.7*year);
+                protein=new BigDecimal(0.9*this.weight);
+            }
+            cholesterol=new BigDecimal(600);
+            fat=energy.multiply(new BigDecimal(0.25*0.111));
+            result[0]=energy.toString().substring(0,6);
+            result[1]=protein.toString();
+            result[2]=cholesterol.toString();
+            result[3]=fat.toString().substring(0,4);
+            return  result;
+        }
+
+    }
+
 }
